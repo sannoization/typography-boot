d577315ba08324fcc3738942373d14800756a9a9
Alexander Sorokin <alexander.sorokin@qligent.com>
2024-07-19 16:55:12 +0300

    remove frontend

--- frontend/src/store/rootSaga.ts
+++
@@ -1,9 +0,0 @@
-import customersSaga from "./sagas/customersSaga";
-import {AllEffect, all} from "redux-saga/effects";
-
-
-export default function* rootSaga(): Generator<AllEffect<any>> {
-  yield all([
-    customersSaga(),
-  ])
-}
--- frontend/src/store/sagas/constants.ts
+++
@@ -1,6 +0,0 @@
-
-
-
-const apiCustomers = (url: string): string => `https://localhost:3030/customers/${url}`;
-export const CUSTOMERS_URL = `https://localhost:3030/customers/`;
-export const CUSTOMER_BY_ID_URL = (id: string) => apiCustomers(id);
